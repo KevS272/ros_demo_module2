f730a42de8cbac078c3f7858a2e0ccbc39af1549
Kevin <kevs272@gmail.com>
2023-09-23 14:07:36 +0200

    correct the entrypoint script

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -11,12 +11,12 @@ if [ ! -e "CONTAINER_INITIALIZED_PLACEHOLDER" ]; then
     touch "CONTAINER_INITIALIZED_PLACEHOLDER" # <== This placeholder file used in the github action to check when colcon build is done, do not remove
     source "/root/ros2_ws/install/setup.bash"
 
-    ros2 ros2 run py_image_foolery image_creator # <== change to your launch file // comment out if you don't want auto launch
+    ros2 run py_image_foolery image_creator # <== change to your launch file // comment out if you don't want auto launch
 else
     echo "-- Not first container startup --"
     source "/root/ros2_ws/install/setup.bash"
 
-    ros2 ros2 run py_image_foolery image_creator # <== change to your launch file // comment out if you don't want auto launch
+    ros2 run py_image_foolery image_creator # <== change to your launch file // comment out if you don't want auto launch
 fi
 
 exec "$@"
